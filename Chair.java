58d24e1e9be266ffe31010cecdde52bdb444eeb8
UsamaRajpoot <muhammadusamashamshair@gmail.com>
2017-03-20 07:14:53 -0700

    Add files via upload

---
+++ Chair.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chair;
+
+/**
+ *
+ * @author Usama Rao
+ */
+public class Chair  
+{
+private int legs;
+private String color;
+
+
+public Chair()
+{
+legs=0;
+color="red";
+
+}
+
+public void setlegs(int l)
+{
+leg=l
+}
+
+public void setcolor(String c)
+{
+color=c;
+}
+ public int getlegs()
+ {
+     return legs;
+ }
+ 
+ public int getcolor()
+ {
+     return color;
+ }
+}
+public class Mainclass
+{
+public static void main (String []args )
+{
+    chair c1=new chair ()
+           c1.setlegs(2);
+           c1.setcolor("red");
+                   system.out.print(c1.getlegs)
+                   system.out.print(c1.getcolor)
+
+                           }
+}
+
+
+
